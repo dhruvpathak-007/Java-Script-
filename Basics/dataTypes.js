f328f483b92d8ea002d17ff413512d8efe58774a
Dhruv Pathak <122620729+dhruvpathak-007@users.noreply.github.com>
2023-12-31 07:42:21 +0000

    data types

---
+++ Basics/dataTypes.js
@@ -0,0 +1,12 @@
+// types of data type in js
+
+//number=>2 to power 53
+//bigint
+//string=>""
+//boolean=>
+//null=> standalone value
+//undefined
+//symbol=>unique
+
+
+//object
--- Basics/variables.js
+++ Basics/variables.js
@@ -1,1 +1,14 @@
-console.log("Hello Dhruv")
+const accounId =14566;
+let accountEmail= "shruv@gmail.com";
+var accountPassword="12345";
+accountCity="Jaipur";
+
+accountEmail="newEmail";
+accountPassword="newp";
+accountCity="newc"
+
+/*
+prefer not use var because of the issue of fun and block scope
+ */
+
+console.table([accounId,accountEmail,accountPassword,accountCity])
